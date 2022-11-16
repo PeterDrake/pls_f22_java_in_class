15ad30d2cce537fd74ca23a56c9a7166f872fbfd
Peter Drake <drake@lclark.edu>
2022-11-16 15:56:42 -0800

    Traffic Light (enum)

---
+++ src/TrafficLightExample.java
@@ -0,0 +1,12 @@
+public class TrafficLightExample {
+    public static void main(String[] args) {
+        TrafficLight light = TrafficLight.GREEN;
+
+        double x = 3;
+        int y = (int)3.5;
+
+        int[][] a = {{1, 2, 3}, {4, 5, 6}};
+
+        int[] b = a[0];
+    }
+}
